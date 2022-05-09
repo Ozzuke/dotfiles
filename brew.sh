19fdb322622ccd2bbbf9c847f67406d4df38d929
Osvald Nigola <osvald@Osvalds-MacBook-Air.local>
2022-05-10 00:19:59 +0300

    fix brew install script

--- brew.sh
+++ brew.sh
@@ -3,11 +3,18 @@
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/osvald/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # install apps
-brew install arduino discord iterm2 malwarebytes messenger obs rectangle surfshark transmission visual-studio-code vlc wacom-tablet zoom zotero
+brew install arduino discord iterm2 malwarebytes messenger obs rectangle spotify surfshark qbittorrent tidal visual-studio-code vlc zoom zotero
 
 # install formulae
-brew install bat cowsay fortune htop lolcat ncdu neofetch powerlevel10k python ranger speedtest-cli task
+brew install bat cowsay fortune htop lolcat ncdu neofetch python ranger speedtest-cli task
+
+# install powerlevel10k for zsh
+brew install romkatv/powerlevel10k/powerlevel10k
+echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
 
 # install from the app store
 echo affinity photo, affinity designer, digidoc4
