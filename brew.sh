919a49c2481c5a1a5add3c478af6979fe8605c31
Osvald <thorin.tammiskilp@gmail.com>
2022-05-09 18:23:56 +0300

    add brew install script

---
+++ brew.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install apps
+brew install arduino discord iterm2 malwarebytes messenger obs rectangle surfshark transmission visual-studio-code vlc wacom-tablet zoom zotero
+
+# install formulae
+brew install bat cowsay fortune htop lolcat ncdu powerlevel10k python ranger speedtest-cli task
+
+# install from the app store
+echo affinity photo, affinity designer, digidoc4
