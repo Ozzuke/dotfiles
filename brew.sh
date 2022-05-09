2acbe8d64a6e1140efe6feaaf23dfd52f021bbaf
Osvald <thorin.tammiskilp@gmail.com>
2022-05-09 19:20:29 +0300

    add neofetch to brew install script

--- brew.sh
+++ brew.sh
@@ -7,7 +7,7 @@
 brew install arduino discord iterm2 malwarebytes messenger obs rectangle surfshark transmission visual-studio-code vlc wacom-tablet zoom zotero
 
 # install formulae
-brew install bat cowsay fortune htop lolcat ncdu powerlevel10k python ranger speedtest-cli task
+brew install bat cowsay fortune htop lolcat ncdu neofetch powerlevel10k python ranger speedtest-cli task
 
 # install from the app store
 echo affinity photo, affinity designer, digidoc4
